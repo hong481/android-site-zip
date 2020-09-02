4d92349ded77d6ecbc37ce2b7ed16d7a57bcab1e
hong481 <irlink2800@irlink.co.kr>
2020-09-02 11:43:37 +0900

    1. glide 로딩 gif 추가. 2. more menu 버튼 click effect 추가.

--- app/src/main/java/kr/co/hongstudio/sitezip/binding/ImageBindingAdapter.kt
+++ app/src/main/java/kr/co/hongstudio/sitezip/binding/ImageBindingAdapter.kt
@@ -2,10 +2,12 @@ package kr.co.hongstudio.sitezip.binding
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.storage.FirebaseStorage
+import kr.co.hongstudio.sitezip.R
 import kr.co.hongstudio.sitezip.glide.GlideApp
 
 object ImageBindingAdapter {
@@ -30,6 +32,7 @@ object ImageBindingAdapter {
                     url
                 }
             )
+            .thumbnail(Glide.with(imageView.context).load(R.raw.gif_loading))
             .fitCenter()
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .apply(RequestOptions.bitmapTransform(CircleCrop()))
